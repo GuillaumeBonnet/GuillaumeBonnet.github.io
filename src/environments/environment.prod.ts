fdd964940314189a8afdb075b714dd2438f1e41c
Guillaume Bonnet <7252481+GuillaumeBonnet@users.noreply.github.com>
2022-02-21 22:06:46 +0100

    update environment.links.personalSite for prod

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: true,
   links: {
     freezer: 'https://freezer-practice.herokuapp.com/api',
-    personalSite: 'http://localhost:4201',
+    personalSite: 'https://guillaumebonnet.github.io',
   },
 };
